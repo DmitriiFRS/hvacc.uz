c325c625efea716198f58c87aee4f040719c4f5f
Дмитрий <Dmitriifrs@gmail.com>
2024-05-23 15:28:50 +0500

    remove video

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,6 @@ export default async function Home() {
    return (
       <>
          <Message />
-         <Video />
          <div className={styles.homepage}>
             <Catalog />
             <Banner img={data.data.banners.nodes[0].bannerGroup.image.node.sourceUrl} />
